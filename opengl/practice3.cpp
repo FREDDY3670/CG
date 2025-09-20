7db3d84cdddd2077cd033ba96dbe397ce03d098c
Kimmingyu <d2rkjc38@gmail.com>
2025-09-21 01:41:43 +0900

    practice1,2 완성본 추가

---
+++ opengl/practice3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <gl/glew.h> 
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid MouseClick(int button, int state, int x, int y);
